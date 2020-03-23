a1035042f55ad24d894fc8e3348fa5c0a52c7801
urvish <urvishp80@gmail.com>
2020-03-23 18:20:47 +0530

    spacing between plots. updated readme and about

--- app/ui.R
+++ app/ui.R
@@ -9,8 +9,14 @@ shinyUI(
         tabPanel("Visualization",
                  sidebarPanel(
                     uiOutput("selectSource"),
-                    actionButton(inputId = "clear_all", label = "Clear selection", icon = icon("check-square")),
-                    actionButton(inputId = "select_all", label = "Select all", icon = icon("check-square-o")),
+                    div(fluidRow(
+                        actionButton(inputId = "clear_all", label = "Clear selection", icon = icon("check-square"),
+                        style='padding:4px; width: 120px')
+                    )),
+                    div(fluidRow(
+                        actionButton(inputId = "select_all", label = "Select all", icon = icon("check-square-o"),
+                        style='padding:4px; width: 120px')
+                        )),
                     div(style = "padding: 10px 0px; margin:10px", fluidRow(
                         radioButtons("askBid",
                                 "Type",
@@ -21,12 +27,16 @@ shinyUI(
                 mainPanel(
                     tabsetPanel( 
                         # Data by 
-                        tabPanel(p(icon("line-chart"), "By Currency"),
+                        tabPanel(p(icon("line-chart"), "Charts"),
                             column(10,
                                     h4('Ask or Bid Price', align = "center"),
                                     plotlyOutput("prices"),
+                                    br(),
+                                    br(),
                                     h4('Ask or Bid Size', align = "center"),
                                     plotlyOutput("size"),
+                                    br(),
+                                    br(),
                                     h4('Spread', align = "center"),
                                     plotlyOutput("spread")
                             )
--- readme.md
+++ readme.md
@@ -1,10 +1,9 @@
 ## FX-Data Visualization
 
-This is sample project to visualize forex data using `Shiny` framework. Shiny is powerful tool and can be used to build many interactive visualization. 
+This is sample project to visualize forex data using `Shiny` framework. Shiny is powerful tool and can be used to build many interactive visualization and applications. 
 
 
-#### Data
-Data has different `ASK` and `BID` prices and other tick data provided by banks. Currently we are not publishing the data. 
+In the dashboard, we are showing what is the `ASK` and `BID` prices and sizes for selected banks. We are also showing the change in the spread over the time for the selected banks. You can also see the raw data which we have used to create such plots by clicking on `Data` tab.  
 
 ## Demo
 The demo of the app can be found [here](https://pirimiddemo.shinyapps.io/fx_spread/).
@@ -24,3 +23,6 @@ The demo of the app can be found [here](https://pirimiddemo.shinyapps.io/fx_spre
 3. Filters
 
     ![Filters](/resources/filters.PNG)
+
+
+Contact us for more such dashboard. 
